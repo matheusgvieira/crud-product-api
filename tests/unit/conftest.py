34c6ac59eb79925fab257bcffe5fba813c623bb8
Matheus Gois Vieira <matheusmaxi9.0@gmail.com>
2023-08-18 01:02:50 -0300

    feat(api) creat test root

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,6 +1,14 @@
 import pytest
+from fastapi.testclient import TestClient
+from products_api.app import app
 
 
 @pytest.fixture(autouse=True)
 def envs_db(monkeypatch):
     monkeypatch.setenv("uri", "")
+
+
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as client:
+        yield client
